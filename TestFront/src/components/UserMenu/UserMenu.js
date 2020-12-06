94ebdd6c5fd528250fd36e58e244b5ed80926eef
plywakd <46456311+plywakd@users.noreply.github.com>
2020-12-06 20:34:58 +0100

    Added react bootstrap button packadge

--- TestFront/src/components/UserMenu/UserMenu.js
+++ TestFront/src/components/UserMenu/UserMenu.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './usermenu.css'
 
 class UserMenu extends React.Component{
@@ -13,11 +15,28 @@ class UserMenu extends React.Component{
 	componentDidMount() {
 	    console.log("Loaded userMenu");
 	}
+	
+	handleLogin() {
+		
+	}
+	
+	handleLaunchGame() {
+		
+	}
+	
+	handleRanking() {
+		
+	}
 
 	render() {
 		return (
 			<div className="user-menu-container">
 				<h4>Hello from userMenu</h4>
+				<div className="btn-container">
+					<Button variant="dark" onClick={this.handleLogin}>Login</Button>
+					<Button variant="dark" onClick={this.handleLaunchGame}>Train Game!</Button>
+					<Button variant="dark" onClick={this.handleRanking}>Ranking</Button>
+				</div>
 			</div>
 		);
 	}
